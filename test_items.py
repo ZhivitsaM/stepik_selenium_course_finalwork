bd957fb8f49bd6185f49c480027556ab9e603913
ZhivitsaM <zhivitsadev@gmail.com>
2025-01-19 00:09:28 +0700

    add conftest.py and test_items.py

---
+++ test_items.py
@@ -0,0 +1,17 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as condition
+
+
+def test_basket_button_is_exists(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    # Проверка наличии кнопки добавления товара в корзину
+    button = WebDriverWait(browser, 10).until(
+        condition.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
+    )
+    assert button, "There is no button to add item to basket!"
+
+    time.sleep(30)
